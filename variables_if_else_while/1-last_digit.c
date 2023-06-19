288b7f2d0af2b1e4c8ba968237dff823da7bec78
aliciastudies <6722@holbertonstudents.com>
2023-06-18 22:40:01 -0700

    fix: line was too long and failed betty check

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,18 +10,18 @@
 int main(void)
 {
 	int n;
-	int lastDigit;
+	int lstDigit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastDigit = n % 10;
+	lstDigit = n % 10;
 
-	if (lastDigit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-	else if (lastDigit < 6 && lastDigit != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+	if (lstDigit > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, lstDigit);
+	else if (lstDigit < 6 && lastDigit != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lstDigit);
 	else
-		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is 0\n", n, lstDigit);
 
 	return (0);
 }
