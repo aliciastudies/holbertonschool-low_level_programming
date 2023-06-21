c0f0711a5326a7c3213813442b502edcbfb850d5
aliciastudies <6722@holbertonstudents.com>
2023-06-20 22:17:47 -0700

    feat: func adds two integers and returns result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *add - adds two integers and returns the result
+ *@a: first integer
+ *@b: second integer
+ *Return: sum of two integers
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
+
+
+
