d492536377a01f5e7cd7a632a75479c08736581b
aliciastudies <6722@holbertonstudents.com>
2023-06-22 02:39:56 -0700

    feat: func prints 0-9 do not print 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *_print_most_numbers - prints numbers from 0-9 without printing 2 and 4
+ *
+ *
+ *Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	n = 0;
+	while (n  <= 9)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar(n + '0');
+		}
+
+		n = n  + 1;
+	}
+
+	_putchar('\n');
+}
