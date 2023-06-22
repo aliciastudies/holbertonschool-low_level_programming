62f14b9b0d58ecb2c3c58f2fb3cc28d501ad8c40
aliciastudies <6722@holbertonstudents.com>
2023-06-22 03:18:58 -0700

    feat: func prints 0-14 ten times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *more_numbers - prints 10 times numbers 0-14
+ *
+ *Return: void
+ */
+
+void more_numbers(void)
+{
+	int c;
+	int d;
+
+	c = 0;
+	while (c <= 10)
+	{
+		d = 0;
+		while (d <= 14)
+		{
+			if (d >= 10 && d <= 14)
+			{
+				_putchar(d / 10 + '0');
+			}
+			_putchar(d % 10 + '0');
+			d = d + 1;
+		}
+		_putchar('\n');
+		c = c + 1;
+	}
+}
+
