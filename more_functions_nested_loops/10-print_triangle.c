1aa212ac61abe4179ccbe0b6ff09374a2b4a772c
aliciastudies <6722@holbertonstudents.com>
2023-06-23 04:59:13 -0700

    why is my triangle still so small

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -32,7 +32,7 @@ void print_triangle(int size)
 	}
 
 	current_line = 0;
-	while (current_line <= size)
+	while (current_line < size)
 	{
 		print_char(' ', (size = size - 1));
 		print_char('#', (current_line = current_line + 1));
