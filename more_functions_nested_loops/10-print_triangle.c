c53f62ef83acd8df821083a17948fbf151a5ad2c
aliciastudies <6722@holbertonstudents.com>
2023-06-23 04:11:50 -0700

    feat: func prints triangle in terminal

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ *print_char - prints x amount of char to match user int input
+ *@to_print: char to print
+ *@how_many_times: the number of times to print char
+ *Return: 0
+ */
+
+void print_char(char to_print, int how_many_times)
+{
+	while (how_many_times > 0)
+	{
+		_putchar(to_print);
+		how_many_times = how_many_times - 1;
+	}
+}
+
+
+/**
+ *end_line - prints a new line
+ *
+ *Return: 0
+ */
+
+void end_line(void)
+{
+	_putchar('\n');
+}
+
+/**
+ *print_triangle - prints a triangle followed by a new line
+ *@size: input from user
+ *Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int current_line;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	current_line = 0;
+	while (current_line < size)
+	{
+		print_char(' ', (size = size - 1));
+		print_char('#', (current_line = current_line + 1));
+		end_line();
+	}
+}
