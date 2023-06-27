96a696c632d497295b9a0a60092494ab560e6c63
aliciastudies <6722@holbertonstudents.com>
2023-06-27 06:22:43 -0700

    doc: comments added

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -17,11 +17,11 @@ void puts_half(char *str)
 	{
 		length = length + 1;
 	}
-	length_of_the_string = (length + 1) / 2;
-	n = (length_of_the_string - 1) / 2;
+	length_of_the_string = (length + 1) / 2; /* + 1 rounds up the division if length is an odd number */
+	n = (length_of_the_string - 1) / 2; /* determines starting point and - 1 ensures `n` represents index of char just before midpoint  */
 	while (length_of_the_string < length)
 	{
-		if (str[length_of_the_string] / 2)
+		if (str[length_of_the_string] / 2) /* non-zero meansindex is odd and char is printed */
 		{
 			_putchar(str[length_of_the_string]);
 		}
