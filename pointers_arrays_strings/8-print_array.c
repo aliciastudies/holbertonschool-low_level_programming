7be5b27b3d8aaa881a725cf883138e9985a725b2
aliciastudies <6722@holbertonstudents.com>
2023-06-26 20:30:36 -0700

    feat: func that prints `n` elements of an array of integers

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_array - prints `n` elements of an array of integers,
+ *followed by a new line
+ *@a: array input from user
+ *@n: integer input from user
+ *Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+	int length;
+
+/*	length = 0;
+	while (a[length] != '\0')
+	{
+		length = length + 1;
+
+	} */
+	length = 0;
+	while (length < n)
+	{
+		if (length == n - 1)
+		{
+			printf("%d", a[length]);
+		}
+		else
+		{
+			printf("%d, ", a[length]);
+		}
+
+		length = length + 1;
+	}
+
+	printf("\n");
+}
