586a88dbaae06ff702c4f8c8aa58dcaf77f7aeb7
aliciastudies <6722@holbertonstudents.com>
2023-06-27 05:38:05 -0700

    doc: comments added

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,12 +13,14 @@ char *_strncpy(char *dest, char *src, int n)
 
 	index = 0;
 
-	while (index < n && src[index] != '\0')
+	while (index < n && src[index] != '\0') /* loop responsible for copying chats from src string to dest string */
 	{
 		dest[index] = src[index];
 		index = index + 1;
 	}
-	while (index < n)
+	while (index < n) /* if there are remaining positions in dest that need to be filled with '\0', second loop is executed to fill in remaining positions */
+			  /* this ensures string isalways  terminated regardless of the length of the source string being copied. */
+			  /* source string could be short than the specified number of char to be copied (`n`) */
 	{
 		dest[index] = '\0';
 		index = index + 1;
