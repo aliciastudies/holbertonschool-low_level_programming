71c623c4b4be591d529a4a5c6fe71bc515132ec6
aliciastudies <6722@holbertonstudents.com>
2023-06-27 06:35:53 -0700

    doc: comments added

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -15,10 +15,10 @@ char *_strcpy(char *dest, char *src)
 
 	while (src[index] != '\0')
 	{
-		dest[index] = src[index];
-		index = index + 1;
+		dest[index] = src[index]; /* dest is the var that is assigned the value so goes on the left of the operator (=) */
+		index = index + 1; /* copies char by char until it reaches null char */
 
 	}
-	dest[index] = '\0';
-	return (dest);
+	dest[index] = '\0'; /* appending null char indicates end of the string */
+	return (dest); /* returns pointer to the des string */
 }
