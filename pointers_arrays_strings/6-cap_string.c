0b9833979b15e84eb84cb385f5a7a8a09effa8fd
aliciastudies <6722@holbertonstudents.com>
2023-06-28 08:57:54 -0700

    doc: added comments

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -20,7 +20,7 @@ int not_alpha(char c)
 			return (1);
 		index = index + 1;
 	}
-	return (0);
+	return (0); /* if loop completes without finding a match, c is not a word separator  */
 }
 
 /**
@@ -37,10 +37,10 @@ char *cap_string(char *s)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if ((s[i] >= 'a' && s[i] <= 'z') && (i == 0))
-			s[i] = s[i] - ('a' - 'A');
-		if ((s[i] >= 'a' && s[i] <= 'z') && (not_alpha(s[i - 1]) == 1))
-			s[i] = s[i] - ('a' - 'A');
+		if ((s[i] >= 'a' && s[i] <= 'z') && (i == 0)) /* checks if current char is lowercase & if first char in string  */
+			s[i] = s[i] - ('a' - 'A'); /* if both conditions are true, it converts lowercase to upper by subtracting with ASCII values  */
+		if ((s[i] >= 'a' && s[i] <= 'z') && (not_alpha(s[i - 1]) == 1)) /* checks if current char is lowercase & checks if precedeing char is a word separator */
+			s[i] = s[i] - ('a' - 'A'); /* returns 1 if it is. ^ compares the return value. if == 1, evaluates to true and current char is a word separator */
 		i = i + 1;
 	}
 	return (s);
