0c97792ca26e9208ac84f03b14e0c872e24367a8
aliciastudies <6722@holbertonstudents.com>
2023-06-29 21:49:33 -0700

    test

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_strpbrk - searches a string for a set of bytes
+ *@s: first string to be scanned
+ *@accept: second string contain the characters to match
+ *
+ * Return: Always 0.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+
+}
