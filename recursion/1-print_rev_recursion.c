2de70a3753323bf1affc101c5423ddc346cdd53f
aliciastudies <6722@holbertonstudents.com>
2023-07-03 01:14:55 -0700

    doc: comments added

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -11,10 +11,20 @@ void _print_rev_recursion(char *s)
 	unsigned char index;
 
 	index = 0;
-	if (s[index] == '\0')
+	if (s[index] == '\0') //
 	{
 		return;
 	}
 	_print_rev_recursion(s + 1);
-	_putchar(s[index]);
+	_putchar(s[index]); /* starts printing when recursion unwinds */
 }
+
+/**
+ * Stack memory used to keep track of function calls.
+ * Follows Last-In-First-Out structure. Most recently added function call
+ * is the first one to be completed.
+ *
+ * Recursion uses stack memory to remember where it left off
+ * in each recursive call
+ * and goes back to previous calls until it reaches the initial recursive call
+ */
