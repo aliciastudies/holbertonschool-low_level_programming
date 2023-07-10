479f94d6efb7ea894068436fc8eb6387dc7d477a
aliciastudies <6722@holbertonstudents.com>
2023-07-09 20:46:52 -0700

    fixed: changed order of operation

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -21,7 +21,7 @@ int _strlen(char *s)
 }
 
 /**
- * strdup - returns a pointer to a newly allocated space in memory,
+ * _strdup - returns a pointer to a newly allocated space in memory,
  * which contains a copy of the string given as a parameter.
  * @str:pointer to char/string to be copied
  *
