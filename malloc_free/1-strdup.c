da5c3d86210fb721f158953fb0647fa8f2965db3
aliciastudies <6722@holbertonstudents.com>
2023-07-09 20:34:37 -0700

    feat: func that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @s: takes parameter pointer to a char
+ *
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	counter = 0;
+	while (s[counter] != '\0')
+	{
+		counter = counter + 1;
+	}
+	return (counter);
+}
+
+/**
+ * strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str:pointer to char/string to be copied
+ *
+ * Return: pointer to duplicated string, NULL if str = 0
+ * or if insufficient memory is available
+ */
+
+char *_strdup(char *str)
+{
+	char *array;
+	int i;
+
+	array = malloc(sizeof(char) * _strlen(str));
+	if (str == NULL || array == NULL)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+	while (array[i] < _strlen(str))
+	{
+		array[i] = str[i];
+		i = i + 1;
+
+	}
+	return (array);
+}
