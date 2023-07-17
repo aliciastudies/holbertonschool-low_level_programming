3db57e39eff468728ecb27c8041fa57e1ca78f2c
aliciastudies <6722@holbertonstudents.com>
2023-07-16 18:31:14 -0700

    feat: func initialises a variable of type `struct dog`

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialises a variable of type `struct dog`
+ * @d: data structure used
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
