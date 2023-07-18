c8cb582e97b2d6be2f7f751cb8685805ea7bc128
aliciastudies <6722@holbertonstudents.com>
2023-07-18 00:28:22 -0700

    feat: a program that performs simple operations

---
+++ function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - return pointer to function
+ *
+ * Return: pointer to function.
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	result = get_op_func(argv[2])(num1, num2);
+
+	printf("%d\n", result);
+
+	return (0);
+
+}
