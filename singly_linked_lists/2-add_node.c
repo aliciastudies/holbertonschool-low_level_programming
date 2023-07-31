9a0e869afe042b776f060e5fe8c242739d91e258
aliciastudies <6722@holbertonstudents.com>
2023-07-31 05:02:51 -0700

    feat: funcs to add a new node at the beginning/end of a list_t list

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to head
+ * @str: string to be added to new node
+ *
+ * Return: pointer of the new element, otherwise NULL if it fails
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+/* allocate new memory for the new node */
+	new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+/* update pointers to add new node at the beginning */
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: pointer to head
+ * @str: string to be added to new node
+ *
+ * Return: pointer of the new element, otherwise NULL if it fails
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	list_t *last_ptr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		last_ptr = *head;
+		while (last_ptr->next != NULL)
+		{
+			last_ptr = last_ptr->next;
+		}
+		last_ptr->next = new_node;
+	}
+	return (new_node);
+}
