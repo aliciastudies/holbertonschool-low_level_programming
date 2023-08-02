0f2fdca17f549677c50c90a36aa1c6f5269e0258
aliciastudies <6722@holbertonstudents.com>
2023-08-02 02:13:37 -0700

    feat: func that returns the number of elements in a linked dlistint_t list

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a list_t list
+ * @h: pointer to list
+ *
+ * Return: the number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes;
+
+	nodes = 0;
+	while (h != NULL)
+	{
+		nodes = nodes + 1;
+		h = h->next;
+	}
+	return (nodes);
+}
