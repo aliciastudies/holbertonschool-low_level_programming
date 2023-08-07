a0f78bd170f8cfc8ee4d3fd52d204bf7c08f2682
aliciastudies <6722@holbertonstudents.com>
2023-08-06 22:26:48 -0700

    update: changed -1 to 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		close(fd);
-		return (-1);
+		return (1);
 	}
 	num = 0;
 	while (text_content[num] != '\0')
