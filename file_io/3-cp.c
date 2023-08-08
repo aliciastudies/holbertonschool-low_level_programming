953a4e2c8a0bd5e08e661ddb871188eb8f2d01d9
aliciastudies <6722@holbertonstudents.com>
2023-08-08 05:13:21 -0700

    update: pass betty

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,22 +7,23 @@
 #include "main.h"
 
 /**
- * read_file -i read from file descriptor
+ * read_file - read from file descriptor
  * @fd_from: file descriptor to be read from
- * @buffer: name of buffer to read in to
+ * @buf: name of buffer to read in to
  * @size: max number of bytes to be read
- * @filename: name of file to be read
+ * @fn: name of file to be read
  * Return: number of bytes read on success, 98 on failure, 0 at end of file
  */
 
 ssize_t read_file(int fd_from, char *buf, int size, char *fn)
 {
 	ssize_t read_file;
+
 	read_file = read(fd_from, buf, size);
 	if (read_file == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fn);
-		exit (98);
+		exit(98);
 	}
 	return (read_file);
 }
@@ -39,7 +40,7 @@ void close_file(int fd)
 	if (close(fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd ");
-		exit (100);
+		exit(100);
 	}
 }
 
@@ -60,15 +61,15 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
-		exit (97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read");
 		dprintf(STDERR_FILENO, " from file %s\n", argv[1]);
-		exit (98);
+		exit(98);
 	}
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
