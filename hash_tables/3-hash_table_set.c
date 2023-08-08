8dfe5a71535fd5c2cd1800c5492018cae831b079
aliciastudies <6722@holbertonstudents.com>
2023-08-08 05:27:30 -0700

    update: added free

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,11 +15,23 @@ hash_node_t *create_new_node(const char *key, const char *value)
 	hash_node_t *new_node;
 
 	new_node = malloc(sizeof(hash_node_t));
-
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->value = strdup(value);
-
-	new_node->next = NULL;
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
 	return (new_node);
 }
 
@@ -43,16 +55,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	index = hash_djb2((unsigned char *)key) % ht->size;
 	new_node = create_new_node(key, value);
-
 	if (new_node == NULL)
 	{
 		return (0);
 	}
 	if (ht->array[index] != NULL)
 	{
-		new_node->next = ht->array[index];
+		if (strcmp(key, ht->array[index]->key) == 0)
+		{
+			ht->array[index]->value = (char *)value;
+			return (1);
+		}
 	}
+	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
