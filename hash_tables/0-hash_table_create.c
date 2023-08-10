271dd15be77efb1806c71100ae7212fa5e48c477
aliciastudies <6722@holbertonstudents.com>
2023-08-09 20:48:07 -0700

    update: switched from type to var in malloc

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -17,12 +17,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		return (NULL);
 	}
-	new_table = malloc(sizeof(hash_table_t));
+	new_table = malloc(sizeof(*new_table));
 	if (new_table == NULL)
 	{
 		return (NULL);
 	}
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	new_table->array = malloc(sizeof(*(new_table->array)) * size);
 	if (new_table->array == NULL)
 	{
 		free(new_table);
